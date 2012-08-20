4d8c3325980575663d3345e36d8642717142e607
rikiji <rikiji@rikiji.it>
2012-08-20 20:46:42 +0200

    initial commit

---
+++ src/it/rikiji/android/redis/Redis.java
@@ -0,0 +1,20 @@
+package it.rikiji.android.redis;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Redis extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_redis);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_redis, menu);
+        return true;
+    }
+}
