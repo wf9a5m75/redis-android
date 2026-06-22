e9c0c41b6e44c86f0912612ede2130db0bcfd0ca
Masashi Katsumata <wf9a5m75@gmail.com>
2026-06-22 17:58:02 +0900

    upgrade gradle to 9.6.0

--- app/src/main/java/example/redis/android/wf9a5m75/exampleapp/MainActivity.java
+++ app/src/main/java/example/redis/android/wf9a5m75/exampleapp/MainActivity.java
@@ -1,10 +1,10 @@
 package example.redis.android.wf9a5m75.exampleapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/example/redis/android/wf9a5m75/exampleapp/RedisService.java
+++ app/src/main/java/example/redis/android/wf9a5m75/exampleapp/RedisService.java
@@ -4,7 +4,6 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.support.annotation.Nullable;
 
 import io.wf9a5m75.redis.RedisAndroid;
 
@@ -15,14 +14,13 @@ public class RedisService extends IntentService {
   }
 
 
-  @Nullable
   @Override
   public IBinder onBind(Intent intent) {
     return null;
   }
 
   @Override
-  protected void onHandleIntent(@Nullable Intent intent) {
+  protected void onHandleIntent(Intent intent) {
 
     Bundle configs = new Bundle();
     configs.putString("protected-mode", "no");  // "no" for demo or development
